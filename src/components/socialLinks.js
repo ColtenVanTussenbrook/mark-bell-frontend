b3b385718f0590a81b4b0156d32badc19b31171e
Colten Tussenbrook <coltenvantussenbrook@gmail.com>
2021-01-08 13:19:21 -0700

    mobile temp fixes

--- src/components/socialLinks.js
+++ src/components/socialLinks.js
@@ -3,28 +3,28 @@ import gitHubLogo from '../assets/images/github.png';
 import linkedInLogo from '../assets/images/linkedin.png';
 import twitterLogo from '../assets/images/twitter.png';
 
-const SocialLinks = (props) => {
+const SocialLinks = () => {
     const gitHub = "https://github.com/ColtenVanTussenbrook";
     const linkedIn = "https://www.linkedin.com/in/coltenvantussenbrook";
     const twitter = "https://twitter.com/coltvant";
 
     return (
-        <ul>
+        <ul className="hidden md:block">
         <a href={gitHub} target="blank">
-            <li className={`text-1xl md:text-2xl hover:underline pr-12 pb-3 ${props.display}`}>
-                <span className="inline-flex mr-2 underline">GitHub</span> 
+            <li className="text-1xl md:text-2xl hover:underline pr-8 md:pr-12 pb-3 inline-block md:block">
+                <span className="inline-flex mr-2 underline hidden md:inline">GitHub</span> 
                 <span className="inline-flex w-4 md:w-8 h-full"><img src={gitHubLogo} alt="GitHub Logo" /></span>
             </li>
         </a>
         <a href={linkedIn} target="blank">
-            <li className={`text-1xl md:text-2xl hover:underline pr-12 pb-3 ${props.display}`}>
-                <span className="inline-flex mr-2 underline">LinkedIn</span> 
+            <li className="text-1xl md:text-2xl hover:underline pr-8 md:pr-12 pb-3 inline-block md:block">
+                <span className="inline-flex mr-2 underline hidden md:inline">LinkedIn</span> 
                 <span className="inline-flex w-4 md:w-8 h-full pt-1"><img src={linkedInLogo} alt="LinkedIn Logo" /></span>
             </li>
         </a>
         <a href={twitter} target="blank">
-            <li className={`text-1xl md:text-2xl hover:underline pr-12 pb-3 ${props.display}`}>
-                <span className="inline-flex mr-2 underline">Twitter</span> 
+            <li className="text-1xl md:text-2xl hover:underline pr-8 md:pr-12 pb-3 inline-block md:block">
+                <span className="inline-flex mr-2 underline hidden md:inline">Twitter</span> 
                 <span className="inline-flex w-4 md:w-8 h-full pt-1"><img src={twitterLogo} alt="Twitter Logo" /></span>
             </li>
         </a>
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ const IndexPage = () => (
     <SEO title="Colten Van Tussenbrook" />
     <div className="md:flex flex-row items-center pt-4 lg:pt-24">
       <div className="pers-info md:w-4/5 md:border-r-4 md:pr-8 border-white">
-        <h1 className="text-gray-500 text-3xl md:text-4xl">Hi, I'm</h1>
-        <h1 className="text-3xl md:text-7xl lg:text-9xl text-white mb-10">Colten<span className="text-gray-500"> Van Tussenbrook</span></h1>
+        <h1 className="text-gray-500 text-4xl">Hi, I'm</h1>
+        <h1 className="text-5xl md:text-7xl lg:text-9xl text-white mb-10">Colten<span className="md:hidden"><br></br></span><span className="text-gray-500"><span className="md:hidden"></span> Van Tussenbrook</span></h1>
         <p className="text-xl md:text-3xl mb-10">I'm a software engineer in <span className="text-gray-500">Salt Lake City</span>. I build modern apps and websites that have a clean design and are easy for people to use.</p>
         <p className="text-lg" >I'm currently taking on freelance projects, <Link to="/freelance">contact me here</Link> if you're looking for a developer!</p>
       </div>
